8e72ebf4c99fe654471829e5d0f7308337063929
Carlos <karlos8122001@gmail.com>
2023-10-09 10:45:29 -0400

    mejorando el componente para los  eventos

--- src/components/layout/EventsDetails.jsx
+++ src/components/layout/EventsDetails.jsx
@@ -5,16 +5,16 @@ export default function EventsDetails() {
   return (
     <>
       {detallesEventosData.active ? (
-        <section className=" w-[90%] xl:w-[80%] h-auto xl:h-auto flex justify-between items-center gap-y-5 flex-col p-2 xl:p-3 rounded-lg  mx-auto md:flex-row md:gap-x-5 lg:h-full lg:mt-16  overflow-hidden">
-          <div className="overflow-hidden md:w-1/2">
+        <section className="mt-10  w-[90%] xl:w-[80%] h-full  flex justify-center items-center gap-y-5 flex-col p-2  xl:p-3 rounded-lg  mx-auto md:flex-row md:gap-x-10  lg:mt-10  ">
+          <div className=" mb-5 overflow-hidden h-screen md:w-1/2 ">
             <img
               src={detallesEventosData.data.image}
               alt="eventos"
-              className=" mx-auto w-full h-full md:h-[100vh] xl:h-[75vh] object-fill object-center"
+              className=" mx-auto w-full h-full object-fill object-center"
             />
           </div>
 
-          <div className="overflow-hidden md:w-1/2 flex items-center flex-col justify-center">
+          <div className="h-full md:w-1/2 flex items-center flex-col justify-center">
             <h1 className="text-xl mb-3 font-bold text-start md:text-lg xl:text-2xl">
               {detallesEventosData.data.title}
             </h1>
