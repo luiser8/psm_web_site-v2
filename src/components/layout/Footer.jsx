33c58ba4ab0c6d0570916be790b1da377b3787f8
Carlos <karlos8122001@gmail.com>
2023-11-07 11:25:51 -0400

    cambiando el tamaño del logo

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <div className="container">
           <div className="flex w-full p-2 flex-col justify-center items-start md:items-start md:flex-row md:justify-evenly">
 
-            <div className="mb-5 max-w-[200px] col-span-1 md:w-1/ md:mr-5">
+            <div className="mb-5 max-w-[110px] col-span-1 md:w-1/ md:mr-5">
               <NavLink to="/" className="mb-3 inline-block">
                 <img
                   src="images/logo/logo.png"
